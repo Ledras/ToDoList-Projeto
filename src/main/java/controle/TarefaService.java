ca36b95cdddc736e176f747d8e438744821a2c25
Leandro Marcelino <leandro.23marcelino@gmail.com>
2025-09-22 23:13:04 -0300

    Update TarefaService.java

--- src/main/java/controle/TarefaService.java
+++ src/main/java/controle/TarefaService.java
@@ -4,3 +4,9 @@ import java.util.List;
 public class TarefaService {
     private List<Tarefa> tarefas = new ArrayList<>();
     private Long id = 1L;
+    
+   public Tarefa criar(String titulo, String descricao) {
+        Tarefa novaTarefa = new Tarefa(id++, titulo, descricao, false);
+        tarefas.add(novaTarefa);
+        return novaTarefa;
+    }
